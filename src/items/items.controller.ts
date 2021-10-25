422e37d2d0984430a755ed41659c5007a727733c
Guide Fari <guideg6@gmail.com>
2021-10-25 08:58:20 +0200

    create & use DTO in post request

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateItemDto } from './DTO/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
   @Get()
   findAll(): string {
     return 'Get all items';
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
   }
 }
